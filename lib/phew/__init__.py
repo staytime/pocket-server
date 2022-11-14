8afa395a50fba7b7d57bd485aec5f523edb5ff43
elliott <staytimeby1964@gmail.com>
2022-11-14 16:09:33 +0800

    Adding my fork of phew package

---
+++ lib/phew/__init__.py
@@ -0,0 +1,48 @@
+__version__ = "0.0.2"
+
+# highly recommended to set a lowish garbage collection threshold
+# to minimise memory fragmentation as we sometimes want to
+# allocate relatively large blocks of ram.
+import gc
+gc.threshold(50000)
+
+# phew! the Pico (or Python) HTTP Endpoint Wrangler
+from . import logging
+
+# helper method to quickly get connected to wifi
+def connect_to_wifi(ssid, password, timeout_seconds=30):
+  import network, time
+
+  logging.debug("> connecting to wifi network:", ssid)
+
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  wlan.connect(ssid, password)
+
+  start = time.ticks_ms()
+  while (time.ticks_ms() - start) < (timeout_seconds * 1000):
+    if wlan.status() < 0 or wlan.status() >= 3:
+      break
+    time.sleep(0.1)
+
+  if wlan.status() != 3:
+    logging.debug("  - failed to connect")
+    return None
+
+  logging.debug("  - ip address: ", wlan.ifconfig()[0])
+
+  return wlan.ifconfig()[0]
+
+def access_point(ssid, password = None):
+  import network
+
+  # start up network in access point mode  
+  wlan = network.WLAN(network.AP_IF)
+  wlan.config(essid=ssid)
+  if password:
+    wlan.config(password=password)
+  else:    
+    wlan.config(security=0) # disable password
+  wlan.active(True)
+
+  return wlan
